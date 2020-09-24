9d3810fff76f820cc7936265f6eae42cc907e7c3
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2020-09-24 11:13:49 +0300

    Update made.js

--- made quizzes/made.js
+++ made quizzes/made.js
@@ -5,7 +5,6 @@ const greetingMassage = document.getElementById("greetMassage");
 const MadeList = document.querySelector(".made-list");
 
 
-
 // Event Listeners
 
 document.addEventListener("DOMContentLoaded", greetUser);
@@ -13,26 +12,28 @@ document.addEventListener("DOMContentLoaded", greetUser);
 
 // Functions
 
-// A function that will greet the user according to the circumstances
+// A function that will greet the user according to the local storage circumstances
 function greetUser(){
     if (Finished_Quizzes === null){
         greetingMassage.innerHTML = "עדיין לא הכנת שאלונים";
     } else {
         greetingMassage.innerHTML = 'בחר שאלון:';
-
+        
         Finished_Quizzes.forEach(ele => {
-            
-            const quizArea = document.createElement("div");
-            quizArea.classList.add('quizArea-item');
+            const quizArea = document.createElement("li");
+            quizArea.classList.add('quizArea');
 
             const quizTag = document.createElement("button");
             quizTag.classList.add('quizTag');
-            quizTag.innerHTML = ele;
+            quizTag.innerHTML = Object.keys(ele);
             quizArea.appendChild(quizTag);
 
             const delButton = document.createElement("button");
             delButton.classList.add("delButton");
+            delButton.innerHTML = 'X';
             quizArea.appendChild(delButton);
+
+            MadeList.appendChild(quizArea);
         });
     }
 };
