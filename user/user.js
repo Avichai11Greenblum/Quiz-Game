8a5e010b74e0a501343fe31643f14f0e354746ac
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2020-10-05 12:18:39 +0300

    Update user.js

--- user/user.js
+++ user/user.js
@@ -9,18 +9,29 @@ const questionNumGUI = document.querySelector(".questionNum");
 const GUIImage = document.getElementById("the-picture");
 const GUIText = document.querySelector(".text");
 
+const answersDiv = document.querySelector(".answers"); 
 const answer1 = document.querySelector(".answer1");
 const answer2 = document.querySelector(".answer2");
 const answer3 = document.querySelector(".answer3");
 const answer4 = document.querySelector(".answer4");
 
+// Global variables
+
+// Bringing up local storage as variables
+const quiz_name = localStorage.getItem("QuizName");
+const questionList = JSON.parse(localStorage.getItem("questionList"));
+let global_index = 0;
+let counter = 0;
+
 // EVENT LISTENERS
 
 homeButton.addEventListener("click", goHome);
-document.addEventListener("DOMContentLoaded", setQuiz)
+document.addEventListener("DOMContentLoaded", setQuiz);
+answersDiv.addEventListener("click", answerClick);
 
 // FUNCTIONS
 
+// A function for going back a page and cleaning the localStorage
 function goHome(e) {
     e.preventDefault();
     
@@ -32,11 +43,9 @@ function goHome(e) {
     window.location.href = "../Home page/Home.html";
 }
 
+// A function that will set the quiz info to the user's screen plus the first question
 function setQuiz() {
-    // Bringing up local storage as variables
-    const quiz_name = localStorage.getItem("QuizName");
-    const questionList = JSON.parse(localStorage.getItem("questionList"));
-
+    
     // Setting the quiz name
     quizNameGUI.innerText = quiz_name;
 
@@ -53,4 +62,65 @@ function setQuiz() {
     answer4.innerText = questionList[0][0].answersBank.fourth;
     // Question Image
     GUIImage.setAttribute("src", questionList[0][0].questionIMG); 
+};
+
+// A function that will determine if the right answer was clicked and will respond appropriately
+function answerClick(e) {
+    e.preventDefault(); // --> prevent button from refreshing page
+
+    // Bringing up local storage as variables
+    const questionList = JSON.parse(localStorage.getItem("questionList"));
+    
+    // receiving user's choice 
+    let userPick;
+    switch(e.target.classList.value) {
+
+        case "answer1":
+            userPick = 1;
+            break;
+
+        case "answer2":
+            userPick = 2;
+            break;
+
+        case "answer3":
+            userPick = 3;
+            break;
+
+        case "answer4":
+            userPick = 4;
+            break;
+    };
+
+    // If the user choose right
+    if (questionList[0][global_index].correctAnswer === userPick) {    
+        e.target.classList.toggle("green");
+        
+        const buttons = [answer1, answer2, answer3, answer4]
+        buttons.forEach(button => button.disabled = !0)
+
+        setTimeout(() => {
+            counter++;
+            e.target.classList.toggle("green");
+            buttons.forEach(button => button.disabled = !1)
+        }, 2000)
+        
+    
+    // If the user chose wrong
+    } else {
+        e.target.classList.toggle("red");
+
+        let x = "answer" + questionList[0][global_index].correctAnswer;
+        console.log(x)
+        eval(x).classList.toggle("green");
+
+        const buttons = [answer1, answer2, answer3, answer4]
+        buttons.forEach(button => button.disabled = !0)
+        
+        setTimeout(() => {
+            e.target.classList.toggle("red");
+            eval(x).classList.toggle("green");
+            buttons.forEach(button => button.disabled = !1)
+        }, 2000)
+    };
 };
